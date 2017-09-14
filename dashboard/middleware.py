1e2d6d461f32867ead3b03219c366f8344d86782
Javi Manzano <javi@potatolondon.com>
2017-09-14 13:09:00 +0100

    Improve middleware

--- dashboard/middleware.py
+++ dashboard/middleware.py
@@ -6,5 +6,12 @@ from django.http import HttpResponseForbidden
 
 class PotatoAuthMiddleware(object):
     def process_request(self, request):
-        if users.get_current_user() is not None and not users.get_current_user().email().endswith('@{}'.format(GOOGLE_APPS_EMAIL_DOMAIN)):
+        from djangae import environment
+        if environment.is_in_task():
+            return
+
+        if environment.is_in_cron():
+            return
+
+        if not users.get_current_user().email().endswith('@{}'.format(GOOGLE_APPS_EMAIL_DOMAIN)):
             return HttpResponseForbidden()
