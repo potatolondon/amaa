e00484fb2967053fa5a73673f053ef9cd65d0b4e
Javi Manzano <javi@potatolondon.com>
2017-09-14 13:07:05 +0100

    Fix middleware

--- dashboard/middleware.py
+++ dashboard/middleware.py
@@ -6,5 +6,5 @@ from django.http import HttpResponseForbidden
 
 class PotatoAuthMiddleware(object):
     def process_request(self, request):
-        if not users.get_current_user().email().endswith('@{}'.format(GOOGLE_APPS_EMAIL_DOMAIN)):
+        if users.get_current_user() is not None and not users.get_current_user().email().endswith('@{}'.format(GOOGLE_APPS_EMAIL_DOMAIN)):
             return HttpResponseForbidden()
